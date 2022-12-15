15ceb9051d2be007e58e7c27cd070e3e1ffd8e78
Lily Cui <lilycui2009@gmail.com>
2022-12-15 14:42:37 -0500

    BaseFiles
    Added base files for frontend

---
+++ main.js
@@ -0,0 +1,42 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+function uploadResume()
+{
+   // var file_data = $("#file_path").prop("files")[0];
+   var file = document.getElementById('file_path').files[0];
+   const reader = new FileReader();
+
+   var file_data;
+   // var file = document.querySelector('#file_path > input[type="file"]').files[0];
+   var encoded_image = getBase64(file).then(
+     data => {
+     console.log(data)
+     var apigClient = apigClientFactory.newClient();
+
+     var body = data;
+     var params = {"key" : file.name, "bucket" : "photoboy", "Content-Type" : file.type};
+     var additionalParams = {};
+     apigClient.postresumeFolderFilePut(params, body , additionalParams).then(function(res){
+       if (res.status == 200)
+       {
+         document.getElementById("uploadText").innerHTML = "Resume Uploaded"
+         document.getElementById("uploadText").style.display = "block";
+       }
+     })
+   });
+
+}
